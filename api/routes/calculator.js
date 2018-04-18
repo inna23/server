db4e433f41fbbc8bc08bce97c3991ee7064f9c31
Inna Oreshkevich <inna.oreshkevich@sitrus.com>
2018-04-18 15:24:11 -0700

    add calculator

--- api/routes/calculator.js
+++ api/routes/calculator.js
@@ -4,16 +4,20 @@ const router = express.Router();
 //Sum
 router.get('/', (req, res, next) => {
   res.status(200).json({
-    message: 'calculator '
+    message: 'calculator'
   })
 });
 
 
 router.post('/sum', (req, res, next) => {
-  let sum = req.body.a + req.body.b;
+  let sum = parseInt(req.body.a, 10) + parseInt(req.body.b, 10);
   res.status(200).json(sum);
 });
 
+router.post('/sub', (req, res, next) => {
+  let sum = parseInt(req.body.a, 10) - parseInt(req.body.b, 10);
+  res.status(200).json(sum);
+});
 
 //
 
